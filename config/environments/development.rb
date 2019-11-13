537e9aa834bf9410983ab6b745b68284730bbfc4
Serge Lafontaine <slafontaine10@gmail.com>
2019-11-12 19:26:45 -0500

    email fixed ENV[]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,10 +46,8 @@ config.action_mailer.smtp_settings = {
  port: 587,
  authentication: 'plain',
  enable_starttls_auto: true,
-# user_name: ENV[‘GMAIL_USERNAME’],
-# password: ENV[‘GMAIL_PASSWORD’],
- user_name: 'marsheswintergolf@gmail.com',
- password: 'RntfagP4me200',
+ user_name: ENV['GMAIL_USERNAME'],
+ password: ENV['GMAIL_PASSWORD'],
 # openssl_verify_mode: ‘none’
   enable_starttls_auto: true }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ ActionMailer::Base.smtp_settings = {
  address: ‘smtp.sendgrid.net’,
  port: “25”,
  domain: ‘heroku.com’,
- user_name: ENV[“SENDGRID_USERNAME”],
- password: ENV[“SENDGRID_PASSWORD”],
+ user_name: ENV['SENDGRID_USERNAME'],
+ password: ENV['SENDGRID_PASSWORD]',
  authentication: ‘plain’,
  enable_starttls_auto: true
 }
