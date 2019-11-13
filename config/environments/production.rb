a6c9ad51485e1b66c70848d128cfaf3a868e4590
Serge Lafontaine <slafontaine10@gmail.com>
2019-11-12 19:44:30 -0500

    fixed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,15 +68,17 @@ config.action_mailer.default_url_options = { :host => 'https://marsheswintergolf
 #config.action_mailer.delivery_method = :smtp
 
 
-config.action_mailer.smtp_settings = {
- address: ‘smtp.sendgrid.net’,
- port: “25”,
- domain: ‘heroku.com’,
- user_name: ENV['SENDGRID_USERNAME'],
- password: ENV['SENDGRID_PASSWORD'],
- authentication: ‘plain’,
- enable_starttls_auto: true
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
 }
+
+
 
 
 
