303bee4b57798ce96693fa5e8c0787c6e1ec1be7
Serge Lafontaine <slafontaine10@gmail.com>
2019-11-12 19:40:37 -0500

    removed config.action_mailer.delivery_method = :smtp from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
  # ActionMailer Config
 config.action_mailer.default_url_options = { :host => 'https://marsheswintergolf.herokuapp.com'}
-config.action_mailer.delivery_method = :smtp
+#config.action_mailer.delivery_method = :smtp
 
 
 config.action_mailer.smtp_settings = {
