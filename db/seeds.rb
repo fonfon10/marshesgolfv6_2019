2735657addc0b1bef5d966d702d377d32c625545
Serge Lafontaine <slafontaine10@gmail.com>
2019-11-28 21:44:44 -0500

    moved time in footer and changed seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -70,8 +70,8 @@ open_close_type_closed = OpenClose.last
 
 
 #day
-day = Date.today
-lastday = Date.new(2020,01,01)
+day = Date.new(2019,12,02)
+lastday = Date.new(2020,05,01)
 
 while day <= lastday do
 	Day.create!(name: day, open_close: open_close_type_open)
